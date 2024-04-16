4ef74117203fc33c5780c0698552abfb267de3c8
Bo Nicolajsen <>
2024-04-16 09:14:45 +0200

    almost done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,30 @@
-// write cool JS hwere!!
+
+/// BRUG ALDRIG /////
+/* var variableOne = 300;
+var variableOne = 20; */
+
+
+
+// let kan ændres, men ikke redeklareres senere
+let variableOne = 300; // datatype number
+let variableTwo = 40000;// datatype string
+
+// const kan ikke ændres eller redeklareres senere
+const myConst = 300;
+
+
+
+
+
+let myArray = ['bo', 'anders', 'svend']//data type Array
+
+
+
+
+let myResult = variableTwo - variableOne * myConst;
+
+
+alert(myArray)
+
+
+
